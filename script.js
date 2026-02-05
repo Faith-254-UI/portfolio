d6b3d4574386ff871cf31df8a05e97f1ccad7e2a
Faith-254-UI <nivafaith@gmail.com>
2026-02-05 22:10:24 +0300

    Added animations and new layout

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-// Show current year in footer
+// Simple script to show the current year automatically in footer
+
 document.addEventListener("DOMContentLoaded", function () {
     const footer = document.querySelector("footer p");
+
     const year = new Date().getFullYear();
+
     footer.innerHTML = "© " + year + " Faith Niva Olesi";
-});
-
-// Scroll animation effect
-window.addEventListener("scroll", function () {
-  let sections = document.querySelectorAll("section");
-
-  sections.forEach(section => {
-    let position = section.getBoundingClientRect().top;
-    let screenPosition = window.innerHeight / 1.3;
-
-    if (position < screenPosition) {
-      section.classList.add("active");
-    }
-  });
+    
 });
